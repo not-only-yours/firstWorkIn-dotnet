1b9cc77f696f876a2b7bed5d09169a9735928df8
Nikita Bakun <not-only-yours@users.noreply.github.com>
2020-07-28 13:49:13 +0300

    work with url and System.Net

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter web addres: ");
+            string url = Console.ReadLine();
+            var uri = new Uri(url);
+            Console.WriteLine(uri.Scheme);
+            Console.WriteLine(uri.Port);
+            Console.WriteLine(uri.Host);
+        }
+    }
+}
